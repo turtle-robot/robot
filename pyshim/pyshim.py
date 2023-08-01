cf3047e82ce78e708896f9b4985c49c26e569611
cTurtle98 <ciaran@cTurtle98.com>
2023-07-31 21:10:52 -0700

    some improvements. the robot now stops the motors when it quits

--- pyshim/pyshim.py
+++ pyshim/pyshim.py
@@ -18,22 +18,33 @@ i2c = busio.I2C(board.SCL, board.SDA)
 mk = MotorKit(address=0x60, i2c=i2c)
 sk = ServoKit(address=0x60, i2c=i2c, channels=16)
 
+def safe():
+     # disable gimbal servos
+    sk.servo[0].angle = None
+    sk.servo[1].angle = None
+    # motor throttle zero
+    mk.motor1.throttle = 0
+    mk.motor2.throttle = 0
+
 def test_throttle_oob(value):
-    if value > 1.0:
-        return True
-    elif value < -1.0:
+    if value > 1.0 or value < -1.0:
         return True
     else:
         return False
 
 def main():
     for line in sys.stdin:
-        cmd = line.split(" ")[0]
+        cmd = line.strip().split(" ")
         match cmd:
-            case "q":
+            case ["q"]:
+                # safe the robot
+                safe()
+                # exit
                 break
-            case ("throttle", left, right, ):
+            case ["throttle", left, right, ]:
                 # test if left throttle is out of bounds, break if true
+                left = -float(left) #TODO flip wires on m1 terminal and remove neg
+                right = float(right)
                 if test_throttle_oob(left):
                     sys.stderr.write("LEFT THROTTLE OUT OF BOUNDS")
                     break
@@ -44,9 +55,11 @@ def main():
                 # set motor throttles, Left=1, Right=2
                 mk.motor1.throttle = left
                 mk.motor2.throttle = right
-            case ("gimbal", pan, tilt):
-                sk.servo[0] = pan
-                sk.servo[1] = tilt
+            case ["gimbal", pan, tilt]:
+                sk.servo[0].angle = float(pan)
+                sk.servo[1].angle = float(tilt)
+            case _:
+                pass
 
 
 
